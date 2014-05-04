ab56796f97124d134aef9f26bab4b2df9e4f8a09
Jerome DeNijs <jadenijs@cs.brown.edu>
2014-05-04 13:27:34 -0400

    not sure what i changed

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/staff_home_page', express.basicAuth('admin', 'fullm0nty'), function(re
     response.render('staff_home_page.html');
 });
 
+app.get('/show_edit_late',function(request,response){
+	response.render('show_edit_late.html');
+})
+
 app.get('/show_creation_success', function(request, response){
     response.render('show_creation_success.html');
 });
